f2d035153acdd36a000e01449eabac57c141071b
akbarrizvi112 <akbarrizvi112@gmail.com>
2026-03-30 23:59:19 +0500

    login update

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -1,4 +1,4 @@
-import { PawPrint } from 'lucide-react';
+import logo from '../../assets/logo.png';
 
 export function AuthLayout({ children }) {
     return (
@@ -6,15 +6,16 @@ export function AuthLayout({ children }) {
             <div className="flex-[0.8] flex flex-col items-center justify-center p-8 lg:p-12 relative overflow-hidden bg-[#ebe6db] lg:border-r border-[#d4cfc4]">
                 <div className="z-10 text-center max-w-md flex flex-col items-center justify-center h-full">
                     <div className="flex flex-col items-center gap-3 mb-8">
-                        <PawPrint className="w-12 h-12 text-[#4a3420]" />
-                        <span className="text-3xl font-serif font-extrabold text-[#4a3420] tracking-tight">Paws&Pour</span>
+                        <img src={logo} alt="Paws & Pour Logo" className="w-72 h-32 object-contain" />
+                        <p className="text-lg text-[#5e6f5e] font-medium hidden lg:block">
+                            Every Pet Matters
+                        </p>
+                        {/* <span className="text-3xl font-serif font-extrabold text-[#4a3420] tracking-tight">Paws&Pour</span> */}
                     </div>
-                    <h1 className="text-4xl lg:text-6xl font-extrabold text-[#4a3420] tracking-tight mb-4">
+                    {/* <h1 className="text-4xl lg:text-6xl font-extrabold text-[#4a3420] tracking-tight mb-4">
                         Welcome<br />Back!
-                    </h1>
-                    <p className="text-lg text-[#5e6f5e] font-medium hidden lg:block">
-                        Smart pet care starts here.
-                    </p>
+                    </h1> */}
+
                 </div>
             </div>
             <div className="flex-[1.2] flex items-center justify-center p-6 lg:p-24 bg-[#ebe6db] lg:bg-[#fdfaf6]">
